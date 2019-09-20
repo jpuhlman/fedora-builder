47425b3eefdc7b269fdc05da597fa7fac72c35d1
jenkins <jpuhlman@mvista.com>
2019-09-20 22:22:00 +0000

    Add a welcome message

--- init.sh
+++ init.sh
@@ -5,5 +5,8 @@ useradd -d $HOME -M -u $USERID -g $USERGID $USERNAME
 echo "$USERNAME  ALL=(ALL)       NOPASSWD: ALL" | tee -a /etc/sudoers > /dev/null
 sudo usermod -a -G mock $USERNAME
 echo "export MOCK_OPTS='--old-chroot'" >> /etc/bashrc
+echo
+echo "Welcome to fedora 30 builder"
+echo
 su - $USERNAME
 
