bc31aaf0c024fcbe11af9e49d487072f75cfd0a1
Jeremy Puhlman <jpuhlman@mvista.com>
2019-10-25 01:58:39 -0400

    Add docker access

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM fedora:30
 RUN yum -y update
 RUN yum groupinstall -y "Development Tools"
-RUN yum install -y sudo vim rpm-build mock screen python python2
+RUN yum install -y sudo vim rpm-build mock screen python python2 docker
 COPY init.sh /
 CMD  bash /init.sh
--- init.sh
+++ init.sh
@@ -8,6 +8,14 @@ groupadd -g $USERGID engr
 useradd -d $HOME -M -u $USERID -g $USERGID $USERNAME
 echo "$USERNAME  ALL=(ALL)       NOPASSWD: ALL" | tee -a /etc/sudoers > /dev/null
 sudo usermod -a -G mock $USERNAME
+if [ -e "/var/run/docker.sock" ] ; then
+   DOCKERGROUP=$(stat -c "%g" /var/run/docker.sock)
+   if [ "$DOCKERGROUP" != "0" ] ; then
+        groupmod -g $DOCKERGROUP docker 2>/dev/null || groupadd -g $DOCKERGROUP docker
+        usermod -aG docker $USERNAME
+   fi
+fi
+
 echo "export MOCK_OPTS='--old-chroot'" >> /etc/bashrc
 echo
 echo "Welcome to fedora 30 builder"
